e7fe70e757d744c0374c0fccb8f40857752a8a05
Kyouma <ffdonkatsu@gmail.com>
2019-01-22 10:37:36 -0200

    Instalação e Configuração Typescript e NPM

---
+++ app/ts/models/Negociacao.ts
@@ -0,0 +1,38 @@
+class Negociacao {
+
+    private _data;
+    private _quantidade;
+    private _valor;
+
+    constructor(data, quantidade,  valor) {
+
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    get data() {
+
+        return this._data;
+
+    }
+
+    get quantidade() {
+
+        return this._quantidade;
+    }
+
+    set quantidade(valor){
+        this._quantidade = valor;
+    }
+
+    get valor() {
+
+        return this._valor;
+    }
+
+    get volume() {
+
+        return this._quantidade * this._valor;
+    }
+}
